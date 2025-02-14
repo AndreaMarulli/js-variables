9db054b9a0df1a22c699add13f2a10e85d07a529
AndreaMarulli <amandreamarulli@gmail.com>
2025-02-14 15:20:52 +0100

    feat: 2 esercizio in completo

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,10 +1,14 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
+const name ='Andrea';
 // Stampa il valore di nome nella console.
+console.log(name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
+const number = 25;
+const bool = true;
 // Stampa tutti i valori nella console.
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
@@ -14,10 +18,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.substring (0,26);
 console.log(numberOfLettersInAlphabet); // Output: 26
