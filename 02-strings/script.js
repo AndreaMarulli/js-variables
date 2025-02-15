bf64500f8147de86b9bca19723f6ef8b35bad750
AndreaMarulli <amandreamarulli@gmail.com>
2025-02-15 10:20:24 +0100

    feat: esercizi corretti

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -4,12 +4,18 @@ const name ='Andrea';
 // Stampa il valore di nome nella console.
 console.log(name);
 
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
-const number = 25;
+const number = 24;
+const text = 'ciao';
 const bool = true;
 // Stampa tutti i valori nella console.
+.console.log(number);
+console.log(text);
+console.log(true);
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -18,10 +24,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = firstName + lastName;
+const fullName = firstName + " " + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = alphabet.substring (0,26);
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
